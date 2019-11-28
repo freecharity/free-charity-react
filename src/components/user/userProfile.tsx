5794a9c6134b4a684036ed31187b896819ffcd1d
Acinate <Acinate@hotmail.com>
2019-11-27 22:30:21 -0500

    added queries to userprofile

--- src/components/user/userProfile.tsx
+++ src/components/user/userProfile.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHistory} from 'react-router';
 import auth from 'util/auth';
+import axios from 'axios';
 
 export default function UserProfile() {
     const history = useHistory();
+    const session: any = sessionStorage.getItem('userSession');
+    const username = session ? JSON.parse(session).username : undefined;
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+
+    useEffect(() => {
+        getCorrectAnswers();
+        console.log(session);
+    }, []);
+
+    const getCorrectAnswers = () => {
+        axios.get(`http://localhost:3000/answers?page=1&deleted=0&correct=0&username=${username}`).then((response) => {
+            const total = response.data.total;
+            if (total) {
+                setCorrectAnswers(total);
+            }
+        });
+    };
 
     const navigate = (url: string) => {
         history.push(url);
@@ -23,10 +41,9 @@ export default function UserProfile() {
                 <div className="avatar">
                     <img src="" alt=""/>
                 </div>
-                <h3 className='text-center'>Jason</h3>
-                <p>You have donated <b>1,000</b> grains of rice.</p>
-                <p>You have answered <b>100</b> questions correctly.</p>
-                <p>Your best streak was <b>52 correct answers in a row.</b></p>
+                <h3 className='text-center'>{username ? username : ''}</h3>
+                <p>You have donated <b>{correctAnswers * 10}</b> grains of rice.</p>
+                <p>You have answered <b>{correctAnswers}</b> questions correctly.</p>
                 <p>Your best subject is Computer Science.</p>
                 <div className="buttons">
                     <button
