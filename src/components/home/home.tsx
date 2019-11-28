06343945a94d6e4d13af6a8880b5dcebb3620313
Acinate <Acinate@hotmail.com>
2019-11-27 21:56:15 -0500

    added homepage stat queries for correct answer and user count

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import HomeLeaderboard from "./homeLeaderboard";
-import HomeDonators from "./homeDonators";
-import HomeAbout from "./homeAbout";
+import React, {useEffect, useState} from 'react';
+import HomeLeaderboard from './homeLeaderboard';
+import HomeDonators from './homeDonators';
+import HomeAbout from './homeAbout';
+import axios from 'axios';
 
 export default function Home() {
+    const [correctAnswers, setCorrectAnswers] = useState(0);
+    const [donations, setDonations] = useState(0);
+    const [userCount, setUserCount] = useState(0);
+
+    useEffect(() => {
+        getCorrectAnswers();
+        getUserCount();
+    }, []);
+
+    const getCorrectAnswers = () => {
+        axios.get('http://localhost:3000/answers?page=1&deleted=0&correct=0').then((response) => {
+            const total = response.data.total;
+            if (total != undefined) {
+                setCorrectAnswers(total);
+            }
+        });
+    };
+
+    const getUserCount = () => {
+        axios.get('http://localhost:3000/users/count').then((response) => {
+            const count = response.data.count;
+            if (count != undefined) {
+                setUserCount(count);
+            }
+        });
+    };
+
     return (
         <div className="home_container">
             <div className="home_inner">
@@ -14,27 +42,27 @@ export default function Home() {
                     <HomeDonators/>
                 </div>
                 <div className="grains">
-                    <h1>101,234,567</h1>
+                    <h1>{correctAnswers * 10}</h1>
                     <p>grains of rice donated</p>
                 </div>
                 <div className="bowls">
-                    <h1>5,342,522</h1>
+                    <h1>{Math.floor((correctAnswers * 10) / 1000)}</h1>
                     <p>bowls filled with rice</p>
                 </div>
                 <div className="meals">
-                    <h1>2,424,523</h1>
+                    <h1>{Math.floor(((correctAnswers * 10) / 1000) / 2)}</h1>
                     <p>meals fed to children</p>
                 </div>
                 <div className="questions">
-                    <h1>10,412,523</h1>
+                    <h1>{(correctAnswers)}</h1>
                     <p>questions answered</p>
                 </div>
                 <div className="donated">
-                    <h1>$523,532</h1>
+                    <h1>${donations}</h1>
                     <p>donated by FreeCharity</p>
                 </div>
                 <div className="users">
-                    <h1>3</h1>
+                    <h1>{userCount}</h1>
                     <p>users signed up</p>
                 </div>
                 <div className="about">
